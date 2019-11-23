781d5e3a4f2253d8b3400f5b49f9c83da5aaf780
Manzi Guevara <manziguevara@gmail.com>
2019-11-23 14:22:56 +0200

    ft(endpoints): viewing specific redflag endpoint [Finishes #169932213]

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,7 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable node/no-unsupported-features/es-syntax */
 import express from 'express';
-import {
-  signUp,
-  signIn,
-  createRedflag
-} from '../middlewares/validation.middleware';
+import { signUp, signIn, createRedflag } from '../middlewares/validation.middleware';
 import userController from '../controllers/userController';
 import redflagController from '../controllers/redflagController';
 import token from '../middlewares/token.middleware';
@@ -14,5 +11,6 @@ route.post('/api/v1/auth/signup', signUp, userController.signup);
 route.post('/api/v1/auth/signin', signIn, userController.signin);
 route.post('/api/v1/red-flags', token, createRedflag, redflagController.create);
 route.get('/api/v1/red-flags', redflagController.viewredflags);
+route.get('/api/v1/red-flags/:id', redflagController.viewSpecificflag);
 
 export default route;
